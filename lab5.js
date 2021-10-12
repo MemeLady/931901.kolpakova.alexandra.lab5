ec10074783e216a6d0ed09ce5920b7b163438fbf
MemeLady <alex.kolpakova2001@gmail.com>
2021-10-13 01:36:08 +0700

    lab5
    лабораторная работа №5

--- lab5.js
+++ lab5.js
@@ -1,7 +1,15 @@
-var dialog = document.querySelector('dialog');
-document.querySelector('#show').onclick = function() {
-  dialog.show();
-};
-document.querySelector('#close').onclick = function() {
-  dialog.close();
+var modalBtns=document.querySelectorAll('.modal-open');
+
+modalBtns.forEach(function(btn){
+	btn.onclick=function(){
+		var modal=btn.getAttribute("data-modal");
+		
+		document.getElementById(modal).style.display="block";
+	};
+});
+
+window.onclick = function(e){
+	if (e.target.className==="modal"){
+		e.target.style.display="none";
+	}
 };
